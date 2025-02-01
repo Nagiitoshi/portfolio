c6a4030ef29010d9ed6fd93ca911a93222ae54d1
Nagi <nagiitoshi@gmail.com>
2025-02-01 17:08:54 -0300

    Sentry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,36 @@
-const { withSentry } = require('@sentry/nextjs');
 
-module.exports = withSentry({
+const nextConfig = {
   reactStrictMode: true,
-  sentry: {
-    // outras configurações do Sentry
-  },
-});
+  sentry: {},
+};
+
+
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+
+    org: "nagi-9z",
+    project: "javascript-nextjs",
+
+
+    silent: !process.env.CI,
+
+    widenClientFileUpload: true,
+
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+
+    tunnelRoute: "/monitoring",
+
+    hideSourceMaps: true,
+
+
+    disableLogger: true,
+
+    automaticVercelMonitors: true,
+  }
+);
